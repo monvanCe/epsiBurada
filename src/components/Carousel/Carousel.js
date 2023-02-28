9df8c0c5ce7ba2db30048daac8d6af67ecc407a3
monvanCe <omerfkoca@gmail.com>
2023-02-28 16:45:05 +0300

    carousel-version-1

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,9 +1,23 @@
+import React from "react";
+
 import "./Carousel.css";
+
 const Carousel = () => {
   return (
-    <div className="carouselContainer">
-      <div className="carouselBody"></div>
-      <div className="carouselFooter"></div>
+    <div className="carousel-container">
+      <div className="carousel-body">
+        <div className="carousel-content-text">
+          <p className="carousel-content-text-light"></p>
+          <p className="carousel-content-text-bold"></p>
+        </div>
+        <div className="carousel-content-image"></div>
+        <button className="carousel-left-button-mobile"></button>
+        <button className="carousel-right-button-mobile"></button>
+      </div>
+      <div className="carousel-footer">
+        <button className="carousel-left-button-desktop"></button>
+        <button className="carousel-right-button-desktop"></button>
+      </div>
     </div>
   );
 };
