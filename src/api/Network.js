8428329ae5d68a5e02259ad5228b2bbdd94118bd
monvanCe <omerfkoca@gmail.com>
2023-05-23 10:32:17 +0300

    constanst api

--- src/api/Network.js
+++ src/api/Network.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { productsApi } from "../constants/api";
 
 export const fetchProducts = async () => {
-  const response = await axios.get("https://fakestoreapi.com/products/");
+  const response = await axios.get(productsApi);
   return response.data;
 };
